b2aac69687d69c710eebe94eb99d6b676d90f0b8
milkcat1994 <milkcat1994@gmail.com>
2020-01-29 22:44:54 +0900

    20_01_29_14888_연산자끼워넣기

---
+++ 0129_J_2604_그릇/src/com/ssafy/Main_J_2604_그릇.java
@@ -0,0 +1,33 @@
+package com.ssafy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+ 
+// 출처 : http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=1865&sca=2050
+public class Main_J_2604_그릇 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 괄호 양식 저장
+        String tempString;
+         
+        // 이전 괄호 형식 저장
+        int prevChar = ' ';
+        int result = 0;
+         
+        tempString = br.readLine();
+        int strSize = tempString.length();
+         
+        // 한칸 씩 확인
+        for(int i = 0; i < strSize; ++i) {
+            if(prevChar != tempString.charAt(i)) {
+                result += 10;
+                prevChar = tempString.charAt(i);
+            }
+            else {
+                result += 5;
+            }
+        }
+        // end for
+        System.out.println(result);
+    }
+}
