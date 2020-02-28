60e421b9584623c8616a01d8ed0cc69d54d5b588
milkcat1994 <milkcat1994@gmail.com>
2020-02-28 10:20:16 +0900

    Code Refactoring SWE_2115_벌꿀채취

--- Individual/Solution_SWE_2115_벌꿀채취.java
+++ Individual/Solution_SWE_2115_벌꿀채취.java
@@ -17,9 +17,6 @@ import java.util.StringTokenizer;
 public class Solution_SWE_2115_벌꿀채취 {
 	public static int N,M,C,tN;
 	public static int[] board;					//꿀량 저장위한 1차원 배열
-	public static boolean[] isBeeSelected;		//벌 선택 위한 방문배열
-	public static boolean[] isHoneySelected;	//꿀통 최대값 위한 방문배열
-	public static Stack<Integer> stack = new Stack<>();
 	public static int[][] bee;	//벌이 선택한 꿀통 번호 저장
 	static int maxResult=0, tempRes=0;
 	
@@ -36,10 +33,8 @@ public class Solution_SWE_2115_벌꿀채취 {
 			M = Integer.parseInt(st.nextToken());
 			C = Integer.parseInt(st.nextToken());
 			tN = N*N;
-			isHoneySelected = new boolean[M];
 			
 			board = new int[tN];
-			isBeeSelected = new boolean[tN];
 			for (int row = 0; row < N; ++row) {
 				st = new StringTokenizer(br.readLine(), " ");
 				for (int col = 0; col < N; ++col) {
@@ -54,11 +49,10 @@ public class Solution_SWE_2115_벌꿀채취 {
 
 				//첫번째 벌
 				for(int j = 0; j < M; ++j) {
-					isBeeSelected[i+j] = true;
 					bee[0][j] = i+j;
 				}
 				
-				getMax(0, 0, 0);
+				getMax(0, 0, 0, 0);
 				beeASum = tempRes;
 				tempRes = 0;
 				for(int j = i+M; j < tN; ++j) {
@@ -67,59 +61,38 @@ public class Solution_SWE_2115_벌꿀채취 {
 					selectedBeeB(j);
 					
 					//A,B다 선택 했다면 해당 정보로 가장 큰 값 고르기
-					getMax(1, 0, 0);
+					getMax(1, 0, 0, 0);
 					beeBSum = tempRes;
 					tempRes = beeASum + beeBSum;
 					maxResult = maxResult > tempRes ? maxResult : tempRes;
 					tempRes = 0;
 				}
-				//선택 취소
-				for(int j = 0; j < M; ++j) {
-					isBeeSelected[i+j] = false;
-				}
 			}
 			System.out.println("#"+t+" "+maxResult);
 		} //end TestCase
 	}
-
-	public static int getHoneySum() {
-		int sum = 0;
-		for(Integer i : stack) {
-			sum += i*i;
-		}
-		return sum;
-	}
 	
-	public static void getMax(int beeN, int index, int count) {
+	public static void getMax(int beeN, int index, int count, int totalSum) {
 		//max값 갱신
 		if(count <= C) {
-			int temp = getHoneySum();
-			tempRes = tempRes > temp ? tempRes : temp;
+			tempRes = tempRes > totalSum ? tempRes : totalSum;
 		}
 		
+		int ti;
 		for(int i = index; i < M; ++i) {
-			if(isHoneySelected[i]) continue;				//이미 선택되었다면 Pass
-			if(count + board[bee[beeN][i]] > C) continue;	//가질수 있는 꿀 량 넘는다면 선택X
-			isHoneySelected[i] = true;
-			stack.push(board[bee[beeN][i]]);
-			getMax(beeN, i+1, count+board[bee[beeN][i]]);
-			isHoneySelected[i] = false;
-			stack.pop();
+			ti = board[bee[beeN][i]];
+			if(count + ti > C) continue;	//가질수 있는 꿀 량 넘는다면 선택X
+			getMax(beeN, i+1, count+ti, totalSum + ti*ti);
 		}
 	}
 	
-	//해당 index부터 벌 선택할 것이다.
+	//두번째 벌
 	public static void selectedBeeB(int index) {
-		//두번째 벌
 		//해당 index에서 M개 뽑을 것임.
-		for(int i = index; i < tN; ++i) {
-			if(isBeeSelected[i]) continue;
-			
-			for(int j = 0; j < M; ++j) {
-				bee[1][j] = i+j;
-			}
-			return;
+		for(int j = 0; j < M; ++j) {
+			bee[1][j] = index+j;
 		}
+		return;
 	}
 	
 	//현재 index에서 M개 뽑을시 다음 row로 넘어가는지 확인
@@ -127,5 +100,4 @@ public class Solution_SWE_2115_벌꿀채취 {
 		if(index/N == (index+M-1)/N) return false;
 		return true;
 	}
-	
 }
