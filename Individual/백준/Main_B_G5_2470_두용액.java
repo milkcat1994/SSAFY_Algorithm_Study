30118dac71a6af7c4203cad0e282df6fdfa69a44
milkcat1994 <milkcat1994@gmail.com>
2020-11-04 23:09:59 +0900

    Code Refactoring 2470_두용액

--- Individual/백준/Main_B_G5_2470_두용액.java
+++ Individual/백준/Main_B_G5_2470_두용액.java
@@ -13,23 +13,20 @@ import java.util.StringTokenizer;
 //출처 : https://www.acmicpc.net/problem/2470
 public class Main_B_G5_2470_두용액 {
 	
-	static final long MAX=2000000000;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
 		int N = Integer.parseInt(br.readLine());
-		List<Long> list = new ArrayList<Long>();
+		List<Integer> list = new ArrayList<>();
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		while(st.hasMoreTokens()) {
-			list.add(new Long(Long.parseLong(st.nextToken())));
+			list.add(Integer.parseInt(st.nextToken()));
 		}
 		
 		Collections.sort(list);
 		
 		int l=0,r=N-1;
-		long diff=MAX, tempL=0;
+		long diff=Integer.MAX_VALUE, tempL=0;
 		//result l,r 위치
 		int rl=0,rr=N-1;
 		
@@ -40,32 +37,20 @@ public class Main_B_G5_2470_두용액 {
 			if(abs(tempL) < diff) {
 				rl = l;	rr = r;
 				diff=abs(tempL);
-				//0보다 크다면 우측을 당길것.
-				if(tempL > 0) {
-					r--;
-				}
-				// 0보다 작다면 좌측을 당길것.
-				else {
-					l++;
-				}
 			}
-			// diff보다 작지 않다면
+			//0보다 크다면 우측을 당길것.
+			if(tempL > 0) {
+				r--;
+			}
+			// 0보다 작다면 좌측을 당길것.
 			else {
-				//0보다 크다면 우측을 당길것.
-				if(tempL > 0) {
-					r--;
-				}
-				// 0보다 작다면 좌측을 당길것.
-				else {
-					l++;
-				}
+				l++;
 			}
 		} // end while(l<r)
 		
 		System.out.println(list.get(rl)+" "+list.get(rr));
 	}
-//	5
-//	-100000000 -2222222 -1111 -12 -1
+
 	public static long abs(long a) {
 		return a>0 ? a : -a;
 	}
