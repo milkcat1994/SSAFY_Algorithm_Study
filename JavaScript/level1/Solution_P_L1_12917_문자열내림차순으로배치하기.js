da78ee29e6bf7b6148d577d5957a8ce783751bab
milkcat1994 <milkcat1994@gmail.com>
2021-01-06 16:05:07 +0900

    Fix : Fix origin url

--- JavaScript/level1/Solution_P_L1_12917_문자열내림차순으로배치하기.js
+++ JavaScript/level1/Solution_P_L1_12917_문자열내림차순으로배치하기.js
@@ -1,4 +1,4 @@
-// 출처 : https://programmers.co.kr/learn/courses/30/lessons/12916
+// 출처 : https://programmers.co.kr/learn/courses/30/lessons/12917
 
 function solution(s) {
     return s.split('').sort((a, b) => a>=b ? -1 : 1).join('');
--- JavaScript/level1/Solution_P_L1_12918_문자열다루기기본.js
+++ JavaScript/level1/Solution_P_L1_12918_문자열다루기기본.js
@@ -1,4 +1,4 @@
-// 출처 : https://programmers.co.kr/learn/courses/30/lessons/12915
+// 출처 : https://programmers.co.kr/learn/courses/30/lessons/12918
 
 function solution(s) {
     return /^\d{6}$|^\d{4}$/g.test(s);
