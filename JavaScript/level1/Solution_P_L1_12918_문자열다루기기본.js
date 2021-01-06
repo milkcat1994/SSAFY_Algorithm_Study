7906ed3a74a26206aa2761c23427afa18d065cdb
milkcat1994 <milkcat1994@gmail.com>
2021-01-06 15:59:14 +0900

    Code Refactoring 21_01_06_L1_문자열다루기기본

--- JavaScript/level1/Solution_P_L1_12918_문자열다루기기본.js
+++ JavaScript/level1/Solution_P_L1_12918_문자열다루기기본.js
@@ -1,7 +1,5 @@
 // 출처 : https://programmers.co.kr/learn/courses/30/lessons/12915
 
 function solution(s) {
-    if(s.length != 4 && s.length != 6)
-        return false;
-    return !/[^0-9]/g.test(s);
+    return /^\d{6}$|^\d{4}$/g.test(s);
 }
