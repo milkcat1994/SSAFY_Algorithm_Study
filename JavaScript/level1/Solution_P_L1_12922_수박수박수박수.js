d8237fd4c389ab5362e40d9708733a3b332bf8b0
milkcat1994 <milkcat1994@gmail.com>
2021-01-06 16:33:21 +0900

    Code Refactoring 21_01_06_L1_수박수박수박수

--- JavaScript/level1/Solution_P_L1_12922_수박수박수박수.js
+++ JavaScript/level1/Solution_P_L1_12922_수박수박수박수.js
@@ -1,6 +1,5 @@
 // 출처 : https://programmers.co.kr/learn/courses/30/lessons/12922
 
 function solution(n) {
-    var arr = Array(Math.floor(n/2)).fill("수박");
-    return arr.join('') + (n%2 == 0 ? "" : "수" );
+    return "수박".repeat(Math.ceil(n/2)).slice(0, n);
 }
