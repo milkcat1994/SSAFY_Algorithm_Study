bf1e26e08a80011548a5422fd09c1b6fb6716770
milkcat1994 <milkcat1994@gmail.com>
2021-01-10 20:11:04 +0900

    Code Refactoring 21_01_10_L1_최대공약수와최소공배수

--- JavaScript/level1/Solution_P_L1_12940_최대공약수와최소공배수.js
+++ JavaScript/level1/Solution_P_L1_12940_최대공약수와최소공배수.js
@@ -4,12 +4,10 @@ function solution(n, m) {
     var gcd;
     var [min, max] = [Math.min(n, m), Math.max(n, m)];
     
-    for(var i=min; i>0; --i){
-        if(!(min%i) && !(max%i)){
-            gcd = i;
-            break;
-        }
+    function getGcd(a, b){
+        return b % a ? getGcd(b%a, a): a;
     }
-    
+
+    gcd = getGcd(min, max);
     return [gcd, n*m/gcd];
 }
