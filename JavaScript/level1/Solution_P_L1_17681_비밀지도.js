b831e9d750dfc8b7e453779498b8f438227787d6
milkcat1994 <milkcat1994@gmail.com>
2021-03-06 14:13:27 +0900

    Code Refactoring 21_03_06_L1_비밀지도

--- JavaScript/level1/Solution_P_L1_17681_비밀지도.js
+++ JavaScript/level1/Solution_P_L1_17681_비밀지도.js
@@ -5,13 +5,11 @@ function solution(n, arr1, arr2) {
     var tmp;
     for(var i=0; i<n; i++){
         tmp = (arr1[i] | arr2[i]).toString(2);
-        answer.push(new Array(n-tmp.toString().length+1).join('0') + tmp+"")
+        answer.push('0'.repeat(n-tmp.length) + tmp+"")
     }
     
     answer = answer.map((str) => {
-        return str.split("").map((character) => {
-            return character == "1" ? "#" : " ";
-        }).join("");
+        return str.replace(/0/g, " ").replace(/1/g, "#");
     })
     return answer;
 }
