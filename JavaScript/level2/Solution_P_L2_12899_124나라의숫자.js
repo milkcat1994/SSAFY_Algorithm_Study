595cfa9f16491bba000266e4f28be1a21e639bdd
milkcat1994 <milkcat1994@gmail.com>
2021-03-09 19:51:32 +0900

    Update 21_03_09_L2_124나라의숫자

--- JavaScript/level2/Solution_P_L2_12899_124나라의숫자.js
+++ JavaScript/level2/Solution_P_L2_12899_124나라의숫자.js
@@ -1,20 +1,16 @@
 // 출처 : https://programmers.co.kr/learn/courses/30/lessons/12899
 
 function solution(n) {
-    var map = {1:1, 2:2, 3:4};
+    var map = [1, 2, 4];
     var answer = [];
     let quotient;
     let remains;
-    while(n>3){
+    while(n>0){
+        n-=1;
         quotient = Math.floor(n/3);
         remains = n%3;
-        if(remains==0){
-            quotient-=1;
-            remains = 3;
-        }
         answer.push(map[remains]);
         n=quotient;
     }
-    answer.push(map[n]);
     return answer.reverse().join("");
 }
