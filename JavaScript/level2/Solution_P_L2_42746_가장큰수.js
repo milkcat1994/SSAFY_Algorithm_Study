09b30494e418d2a97c8bad493b0e3e7ebcaf33fc
milkcat1994 <milkcat1994@gmail.com>
2021-03-10 14:08:58 +0900

    Update 21_03_10_L2_42746_가장큰수

--- JavaScript/level2/Solution_P_L2_42746_가장큰수.js
+++ JavaScript/level2/Solution_P_L2_42746_가장큰수.js
@@ -1,17 +1,8 @@
 // 출처 : https://programmers.co.kr/learn/courses/30/lessons/42746
 
 function solution(numbers) {
-    let aLength,bLength;
     var answer = numbers.map((num) => num+"")
-        .sort((a,b) => {
-            aLength = a.length;
-            bLength = b.length;
-            let maxLength = aLength + bLength;
-            for(let idx=0; idx<maxLength; idx++){
-                if(a.charAt(idx%aLength) == b.charAt(idx%bLength)) continue;
-                return b.charAt(idx%bLength) - a.charAt(idx%aLength);
-            }
-            return 0;
-        });
-    return answer.join("").replace(/^0+/,'0');
+        .sort((a,b) => (b+a)*1 - (a+b)*1)
+        .join("");
+    return answer.replace(/^0+/,'0');
 }
