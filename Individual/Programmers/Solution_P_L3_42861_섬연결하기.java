7a06404617c3700cd3aac8ebbc9d392a22385d64
milkcat1994 <milkcat1994@gmail.com>
2021-04-25 18:58:26 +0900

    Code_Refactoring_L3_42861_섬연결하기_JAVA

--- Individual/Programmers/Solution_P_L3_42861_섬연결하기.java
+++ Individual/Programmers/Solution_P_L3_42861_섬연결하기.java
@@ -1,11 +1,9 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 /*
  * -섬 연결하기-
  * 1. union-find
+ * 2. List 대신 costs를 변결하면 간단
  * 
  * 풀이 시간 : 15M
  */
@@ -15,19 +13,18 @@ public class Solution_P_L3_42861_섬연결하기 {
 	int[] parents;
 	public int solution(int n, int[][] costs) {
 		int answer=0;
-		initParent(n);
-		
-		List<Edge> list = new ArrayList<>();
-		
-		for(int[] arr : costs) {
-			list.add(new Edge(arr[0], arr[1], arr[2]));
+		int[][] costsCopy = new int[costs.length][3];
+		for(int i=0; i<costs.length; ++i) {
+			costsCopy[i] = costs[i].clone();
 		}
 		
-		Collections.sort(list, (e1, e2) -> Integer.compare(e1.w, e2.w));
+		initParent(n);
 		
-		for(Edge edge : list) {
-			if(union(edge.s, edge.e))
-				answer+=edge.w;
+		Arrays.sort(costsCopy, (e1, e2) -> Integer.compare(e1[2], e2[2]));
+		
+		for(int idx=0; idx<costsCopy.length; ++idx) {
+			if(union(costsCopy[idx][0], costsCopy[idx][1]))
+				answer+=costsCopy[idx][2];
 		}
 		
 		return answer;
@@ -51,15 +48,6 @@ public class Solution_P_L3_42861_섬연결하기 {
 		
 		parents[pa] = pb;
 		return true;
-	}
-	
-	class Edge{
-		int s,e,w;
-		Edge(int s, int e, int w){
-			this.s=s;
-			this.e=e;
-			this.w=w;
-		}
 	}
 
 	public static void main(String[] args) {
